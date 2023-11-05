096535bcc458e8ee3f4ab5753202e4563ed65d39
kimsva <svarstad@gmail.com>
2023-11-05 22:30:29 +0100

    alle endpoints lagt til

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request, abort
 from neo4j import GraphDatabase, exceptions
 
+app = Flask(__name__)
+
 URI = "neo4j+ssc://84c9b254.databases.neo4j.io"
 USERNAME = "neo4j"
 PASSWORD = "9BPTWpu5kp2nWjPyfuhfn5zorZKBrh5jBpKC3uBw908"
@@ -69,7 +71,7 @@ def cancel_order_car():
 
     # Input validation to ensure needed data is present
     if not customer_id or not car_id:
-        return jsonify({"success:" False, "message": "Missing customer_id or car_id"}), 400
+        return jsonify({"success": False, "message": "Missing customer_id or car_id"}), 400
 
     try: 
         with get_db_session() as session:
@@ -98,9 +100,98 @@ def cancel_order_car():
         else:
             #If cancellation unsuccessful
             return jsonify({"success": False, "message": "Cancellation failed"}), 400
-        except exceptions.Neo4jError as e:
-            #If neo4j related error with transaction respond with 500 error
-            abort(500, description=f"Transaction failed: {str(e)}")
+    except exceptions.Neo4jError as e:
+        #If neo4j related error with transaction respond with 500 error
+        abort(500, description=f"Transaction failed: {str(e)}")
+
+
+@app.route("/rent-car", methods=['POST'])
+def rent_car():
+    # Extract customer and car IDs from request JSON body
+    data = request.json
+    customer_id = data.get('customer_id')
+    car_id = data.get('car_id')
+
+    # Input validation
+    if not customer_id or not car_id:
+        return jsonify({"success": False, "message": "Missing customer_id or car_id"}), 400
+
+    try:
+        with get_db_session() as session:
+            # Transaction to rent a car for a customer
+            result = session.write_transaction(
+                lambda tx: tx.run(
+                    """
+                    // Match the customer with the given customer_id
+                    MATCH (customer:Customer {id: $customer_id})
+                    // Match the car which is booked by the customer
+                    MATCH (customer)-[r:BOOKED]->(car:Car {id: $car_id})
+                    // If the car is booked, set its status to rented
+                    SET car.status = 'rented'
+                    // Remove the BOOKED relationship as the car is now rented
+                    DELETE r
+                    // Return whether the operation was successful
+                    RETURN count(car) > 0 as rented
+                    """,
+                    customer_id=customer_id,
+                    car_id=car_id
+                ).single()
+            )
+        # If the car was successfully rented, return a success response
+        if result and result["rented"]:
+            return jsonify({"success": True, "message": "Car rented successfully."}), 200
+        else:
+            # If renting was unsuccessful, either due to the car not being booked by this customer
+            return jsonify({"success": False, "message": "Car renting failed or the car is not booked by this customer."}), 400
+    except exceptions.Neo4jError as e:
+        # Handle any Neo4j-related error
+        abort(500, description=f"Transaction failed: {str(e)}")
+
+    return jsonify({"success": False, "message": "Unexpected error occurred"}), 500
+
+
+@app.route("/return-car", methods=['POST'])
+def return_car():
+    # Extract customer and car IDs, and return status from request JSON body
+    data = request.json
+    customer_id = data.get('customer_id')
+    car_id = data.get('car_id')
+    return_status = data.get('return_status')  # should be 'ok' or 'damaged'
+
+    # Input validation
+    if not customer_id or not car_id or return_status not in ['ok', 'damaged']:
+        return jsonify({"success": False, "message": "Invalid or missing customer_id, car_id, or return_status"}), 400
+
+    try:
+        with get_db_session() as session:
+            # Transaction to return a rented car
+            result = session.write_transaction(
+                lambda tx: tx.run(
+                    """
+                    // Match the customer and rented car
+                    MATCH (customer:Customer {id: $customer_id})-[r:RENTED]->(car:Car {id: $car_id})
+                    // Remove the RENTED relationship and update car's status based on return_status
+                    DELETE r
+                    SET car.status = CASE $return_status WHEN 'ok' THEN 'available' ELSE 'damaged' END
+                    // Return whether the operation was successful
+                    RETURN count(car) > 0 as returned
+                    """,
+                    customer_id=customer_id,
+                    car_id=car_id,
+                    return_status=return_status
+                ).single()
+            )
+        # If the car was successfully returned, return a success response
+        if result and result["returned"]:
+            return jsonify({"success": True, "message": "Car returned successfully."}), 200
+        else:
+            # If returning was unsuccessful, possibly because the car was not rented
+            return jsonify({"success": False, "message": "Car return failed or car was not rented."}), 400
+    except exceptions.Neo4jError as e:
+        # Handle any Neo4j-related error
+        abort(500, description=f"Transaction failed: {str(e)}")
+
+    return jsonify({"success": False, "message": "Unexpected error occurred"}), 500
             
 
 if __name__ == '__main__':
